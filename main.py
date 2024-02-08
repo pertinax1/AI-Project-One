3aa25ad3c8acbbe6019ea1af9e90a2907cf82277
Codesseus <cody@ccushing.com>
2024-02-07 21:32:39 -0700

    Added the code for creating churn ratio dataframes for discrete data

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
+import dataframe_utils
 import pandas as pd
 
+"""
+TODO: The current scripts in here are for testing, remove if wanted
+"""
+test_df = pd.read_csv("personal_resources/account_predict_data.csv")
+print(test_df.head())
 
-def compute_churn_ratio_discrete(note_churn_df):
-    """
-    This function takes a column of note from a data frame and the churn column from that data frame,
-    and returns a data frame with the churn ratio for each discrete/unique entry in the original column.
+list_of_columns = ["gender", "contract_duration", "payment_method"]
 
-    Note: churn ratio = churn sum/column count
+ratio_dfs_list = dataframe_utils.create_churn_ratio_dataframes(test_df, list_of_columns)
 
-    inputs:
-        -note_churn_df: a data frame that contains just the column of interest and the churn value columns
-    return:
-        -churn_ratio_df: data frame that contains all the ratios for the discrete columns 
-    """
-
-    #drop na
-
-    #get list of all the unique values
-
-    #loop through the unique values and group by those values
-
-        #sum the churn count
-        # divide the churn count by the sum of the churn columns
-        # could probably do this as part of the group by with a lamda and the agg function
-
-    #recombine the specific value dfs to create the churn_ratio_df
-
-    #TODO: set this to an actual value
-    churn_ratio_df = None
-    return churn_ratio_df
-    
+for ratio_df in ratio_dfs_list:
+    print(ratio_df.head())
